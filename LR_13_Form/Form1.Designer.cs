8c271d988a2d8f13539f719eeb7d1528ba77cbcc
Kartorez <Bmatvienko05@gmail.com>
2024-04-20 15:22:47 +0300

    Добавьте файлы проекта.

---
+++ LR_13_Form/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace LR_13_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.TextSurname = new System.Windows.Forms.TextBox();
+            this.TextName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DateOfBirth
+            // 
+            this.DateOfBirth.Location = new System.Drawing.Point(204, 148);
+            this.DateOfBirth.MaxDate = new System.DateTime(2020, 12, 31, 0, 0, 0, 0);
+            this.DateOfBirth.MinDate = new System.DateTime(1930, 1, 1, 0, 0, 0, 0);
+            this.DateOfBirth.Name = "DateOfBirth";
+            this.DateOfBirth.Size = new System.Drawing.Size(200, 20);
+            this.DateOfBirth.TabIndex = 1;
+            this.DateOfBirth.Value = new System.DateTime(2020, 12, 31, 0, 0, 0, 0);
+            this.DateOfBirth.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // TextSurname
+            // 
+            this.TextSurname.Location = new System.Drawing.Point(204, 77);
+            this.TextSurname.MaxLength = 20;
+            this.TextSurname.Name = "TextSurname";
+            this.TextSurname.Size = new System.Drawing.Size(200, 20);
+            this.TextSurname.TabIndex = 2;
+            this.TextSurname.TextChanged += new System.EventHandler(this.TextSurname_TextChanged);
+            this.TextSurname.Validating += new System.ComponentModel.CancelEventHandler(this.TextSurname_Validating);
+            this.TextSurname.Validated += new System.EventHandler(this.TextSurname_Validated);
+            // 
+            // TextName
+            // 
+            this.TextName.Location = new System.Drawing.Point(204, 114);
+            this.TextName.MaxLength = 20;
+            this.TextName.Name = "TextName";
+            this.TextName.Size = new System.Drawing.Size(199, 20);
+            this.TextName.TabIndex = 3;
+            this.TextName.TextChanged += new System.EventHandler(this.TextName_TextChanged);
+            this.TextName.Validating += new System.ComponentModel.CancelEventHandler(this.TextName_Validating);
+            this.TextName.Validated += new System.EventHandler(this.TextName_Validated);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введіть Ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введіть Прізвище";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 249);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextName);
+            this.Controls.Add(this.TextSurname);
+            this.Controls.Add(this.DateOfBirth);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LR_13";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker DateOfBirth;
+        private System.Windows.Forms.TextBox TextSurname;
+        private System.Windows.Forms.TextBox TextName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ LR_13_Form/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LR_13_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextSurname_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+        public bool ValidText(string Text, out string ErrorMess) {
+            if (Text.Length == 0)
+            {
+                ErrorMess = "Введіть текст";
+                return false;
+            }
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (char.IsNumber(Text[i]))
+                {
+                    ErrorMess = "Невірний формат";
+                    return false;
+                }
+            }
+            ErrorMess = "";
+            return true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime DateNow = DateTime.Now;
+            int YearNow = DateNow.Year;
+            int YearBirth = DateOfBirth.Value.Year;
+            int Age = YearNow - YearBirth;
+            DateTime NextBirthday = new DateTime(DateNow.Year, DateOfBirth.Value.Month, DateOfBirth.Value.Day);
+            TimeSpan timeUntilNextBirthday = NextBirthday - DateNow;
+            int DaysToNextBirthday = (int)timeUntilNextBirthday.TotalDays;
+            if (DateNow.Date.Day == DateOfBirth.Value.Date.Day && DateNow.Date.Month == DateOfBirth.Value.Date.Month) {
+                MessageBox.Show($"Вітаємо у вас сьогодні день народження! Вам виповнилось виповнилось {Age} років");
+
+            }
+            else if (DaysToNextBirthday > 0)
+            {
+                MessageBox.Show($"До дня народження залишилось {DaysToNextBirthday} днів вам виповнилось {Age} років");
+            }
+            else
+            {
+                DaysToNextBirthday= 365 + DaysToNextBirthday;
+                MessageBox.Show( $"До дня народження залишилось {DaysToNextBirthday} днів вам виповнилось {Age} років");
+            }
+
+            
+        }
+
+        private void TextName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextSurname_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMessage;
+            if (!ValidText(TextSurname.Text, out errorMessage))
+            {
+                e.Cancel = true;
+                TextSurname.Select(0, TextSurname.Text.Length);
+
+                this.errorProvider1.SetError(TextSurname, errorMessage);
+            }
+        }
+
+        private void TextName_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMessage;
+            if (!ValidText(TextName.Text, out errorMessage))
+            {
+                e.Cancel = true;
+                TextSurname.Select(0, TextName.Text.Length);
+
+                this.errorProvider1.SetError(TextName, errorMessage);
+            }
+
+        }
+
+        private void TextSurname_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(TextSurname, "");
+           
+        }
+
+        private void TextName_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(TextName, "");
+        }
+    }
+}
